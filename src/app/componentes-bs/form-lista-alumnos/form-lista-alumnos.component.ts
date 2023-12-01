94b31582310f925be49d95bdd75f461070752a99
cuartociclo2023 <cuartocicloidat2023@gmail.com>
2023-11-30 21:12:39 -0500

    ejemplo pipes

--- src/app/componentes-bs/form-lista-alumnos/form-lista-alumnos.component.ts
+++ src/app/componentes-bs/form-lista-alumnos/form-lista-alumnos.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrl: './form-lista-alumnos.component.css'
 })
 export class FormListaAlumnosComponent {
+  mostrarAlumnos = 0;
+  alumnos = [
+    {nombre: 'Karla Miranda'},
+    {nombre: 'Jesús Orbeso'},
+    {nombre: 'María Gomez'},
+    {nombre: 'Felix Romero'},
+  ]
+  cursos = [
+    {nombre: "Desarrollo de Aplicaciones Móviles", precio: 8000},
+    {nombre: "Administración de base de datos", precio: 5000},
+    {nombre: "Desarrollo de Aplicaciones Web", precio: 6500},
+    {nombre: "Desarrollo de Servicio Web", precio: 3000},
+    {nombre: "Desarrollo de Aplicaciones Móviles iOS", precio: 10000}
+  ]
+  fechaActual = new Date()
+
+  showStudents(){
+    this.mostrarAlumnos = 1;
+  }
+  showCourses(){
+    this.mostrarAlumnos = 0;
+  }
 
 }
