819bca104366aa27c8142fd4de9155b13f575330
cuartociclo2023 <cuartocicloidat2023@gmail.com>
2023-11-30 20:46:57 -0500

    lógica del componente IMC

--- src/app/componentes-bs/form-calculo-imc/form-calculo-imc.component.ts
+++ src/app/componentes-bs/form-calculo-imc/form-calculo-imc.component.ts
@@ -7,4 +7,35 @@ import { Component } from '@angular/core';
 })
 export class FormCalculoImcComponent {
 
+  formImc = {
+    nombre: null,
+    peso: null,
+    talla: null,
+    valorimc: 0
+  }
+  mostrarResultado = false;
+  listPacientes: Paciente = [];
+
+  calcularImc(){
+    let tallam = Number(this.formImc.talla)/100;
+    this.formImc.valorimc = Number(this.formImc.peso) / (tallam * tallam);
+    let status = ""
+    if(this.formImc.valorimc <= 18.5){
+      status = "Por debajo del peso."
+    }else if(this.formImc.valorimc <= 25){
+      status = "Peso normal."
+    }else if(this.formImc.valorimc <= 30){
+      status = "Sobrepeso."
+    }else if(this.formImc.valorimc <= 35 ){
+      status = "Obesidad leve."
+    }else if(this.formImc.valorimc <= 39){
+      status = "Obesidad media."
+    }else {
+      status = "Obesidad mórbida."
+    }
+    this.listPacientes.push({nombre: String(this.formImc.nombre), valorimc: this.formImc.valorimc, status: status})
+  }
+
+
 }
+type Paciente = Array<{nombre: string, valorimc: number, status: string}>;
